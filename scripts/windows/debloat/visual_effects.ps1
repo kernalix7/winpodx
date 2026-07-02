73600a441396731f135622ec0dcd4dc353886b71
GameSoul7Eugene <59319082+GameSoul7Eugene@users.noreply.github.com>
2026-07-03 00:02:20 +0200

    Update visual_effects.ps1

--- scripts/windows/debloat/visual_effects.ps1
+++ scripts/windows/debloat/visual_effects.ps1
@@ -1,24 +1,47 @@
 # SPDX-License-Identifier: MIT
-# winpodx debloat: visual effects -> "best performance" (sysguides recipe).
+# winpodx debloat: visual effects -> "best performance" and other tweaks for more responsiveness
 #
-# Sets VisualFXSetting=2 (custom), then explicitly enables the three
-# settings sysguides recommends keeping ON for readability:
+# Sets VisualFXSetting=3 (custom), then explicitly enables five settings for better readability:
 #   * Show thumbnails instead of icons
-#   * Show window contents while dragging
 #   * Smooth edges of screen fonts
-# Everything else (animations, fades, taskbar transitions) goes off.
+#   * Show shadows under windows (for better visibility of white windows when they are present all over the screen)
+#   * Show translucent selection rectangle (it's inconsistent and applies only to the Desktop, not the rest of apps like File Explorer – imo it's better to have consistent rectangle for everything)
+#   * Use drop shadows for icon labels on the desktop (for better visibility of desktop items with some light background applied)
+#
+# Everything else (animations, fades, taskbar transitions) goes off,
+# including "Show window contents while dragging", because it is somehow disabled when Windows is installed (whatever dockur/windows is applying in the answer file)
+# and to try to diagnose an issue, when windows with no control buttons / just small close one aren't normally draggable while in FreeRDP.
 
-Write-Host "[visual_effects] Setting VisualFXSetting -> 'custom'..."
-New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Force -ErrorAction SilentlyContinue | Out-Null
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Value 3 -Type DWord -Force -ErrorAction SilentlyContinue
+Write-Host "[visual_effects] Disabling animations + applying other responsive tweaks..."
 
-Write-Host "[visual_effects] Disabling animations + taskbar transitions..."
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Value ([byte[]](0x90,0x12,0x03,0x80,0x10,0x00,0x00,0x00)) -Type Binary -Force -ErrorAction SilentlyContinue
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" -Name "MinAnimate" -Value "0" -Type String -Force -ErrorAction SilentlyContinue
+$perfValues = @(
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects"; Name="VisualFXSetting"; Value=3},
+	@{Path="HKCU:\Control Panel\Desktop"; Name="UserPreferencesMask"; Value=([byte[]](0x90,0x12,0x07,0x80,0x10,0x00,0x00,0x00))},
+	@{Path="HKCU:\Control Panel\Desktop"; Name="FontSmoothing"; Value="2"},
+	@{Path="HKCU:\Control Panel\Desktop"; Name="DragFullWindows"; Value="0"},
+	@{Path="HKCU:\Control Panel\Desktop\WindowMetrics"; Name="MinAnimate"; Value="0"},
+	@{Path="HKCU:\Software\Microsoft\Windows\DWM"; Name="EnableAeroPeek"; Value=0},
+	@{Path="HKCU:\Software\Microsoft\Windows\DWM"; Name="AlwaysHibernateThumbnails"; Value=0},
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="TaskbarAnimations"; Value=0},
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="IconsOnly"; Value=0},
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="ListviewShadow"; Value=1},
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="ListviewAlphaSelect"; Value=1},
+	
+	# Misc.
+	# Set shortest keyboard character repeat delay – really useful while typing!
+    @{Path="HKCU:\Control Panel\Keyboard"; Name="KeyboardDelay"; Value="0"},
+	
+	# Accelerate previews appearance of taskbar items
+	@{Path="HKCU:\Control Panel\Mouse"; Name="MouseHoverTime"; Value="20"},
+	
+	# Accelerate frequency of cursor flickering
+	@{Path="HKCU:\Control Panel\Desktop"; Name="CursorBlinkRate"; Value="250"},
+	
+	# Remove delay of appearance of context menu
+	@{Path="HKCU:\Control Panel\Desktop"; Name="MenuShowDelay"; Value="20"}
+)
 
-Write-Host "[visual_effects] Disabling taskbar animations..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAnimations" -Value 0 -Type DWord -Force -ErrorAction SilentlyContinue
-
-Write-Host "[visual_effects] Disabling listview shadow + fade out..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewShadow" -Value 0 -Type DWord -Force -ErrorAction SilentlyContinue
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewAlphaSelect" -Value 0 -Type DWord -Force -ErrorAction SilentlyContinue
+foreach ($item in $perfValues) {
+    New-Item -Path $item.Path -Force -ErrorAction SilentlyContinue | Out-Null
+    Set-ItemProperty -Path $item.Path -Name $item.Name -Value $item.Value -Force -ErrorAction SilentlyContinue
+}
