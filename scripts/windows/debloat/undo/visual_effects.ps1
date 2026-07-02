7dcdd9317ea1a98eaa86af0fcc2df844efd6c9b7
GameSoul7Eugene <59319082+GameSoul7Eugene@users.noreply.github.com>
2026-07-03 00:03:06 +0200

    Update visual_effects.ps1

--- scripts/windows/debloat/undo/visual_effects.ps1
+++ scripts/windows/debloat/undo/visual_effects.ps1
@@ -1,16 +1,29 @@
 # SPDX-License-Identifier: MIT
-# winpodx debloat UNDO: visual effects -> "let Windows decide".
+# winpodx debloat UNDO: visual effects -> "let Windows decide" and other tweaks for more responsiveness
 
-Write-Host "[visual_effects] Restoring VisualFXSetting -> 'let Windows decide'..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Value 0 -Type DWord -Force -ErrorAction SilentlyContinue
+Write-Host "[visual_effects] Restoring animations + other responsive tweaks..."
 
-Write-Host "[visual_effects] Restoring animation defaults..."
-# The 0x9E,0x1E,0x07,0x80,0x12,0x00,0x00,0x00 byte sequence is the Windows
-# default UserPreferencesMask with full animations.
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Value ([byte[]](0x9E,0x1E,0x07,0x80,0x12,0x00,0x00,0x00)) -Type Binary -Force -ErrorAction SilentlyContinue
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" -Name "MinAnimate" -Value "1" -Type String -Force -ErrorAction SilentlyContinue
+$perfValues = @(
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects"; Name="VisualFXSetting"; Value=0},
+	@{Path="HKCU:\Control Panel\Desktop"; Name="UserPreferencesMask"; Value=([byte[]](0x9E,0x1E,0x07,0x80,0x12,0x00,0x00,0x00))},
+	@{Path="HKCU:\Control Panel\Desktop"; Name="FontSmoothing"; Value="2"},
+	@{Path="HKCU:\Control Panel\Desktop"; Name="DragFullWindows"; Value="1"},
+	@{Path="HKCU:\Control Panel\Desktop\WindowMetrics"; Name="MinAnimate"; Value="1"},
+	@{Path="HKCU:\Software\Microsoft\Windows\DWM"; Name="EnableAeroPeek"; Value=1},
+	@{Path="HKCU:\Software\Microsoft\Windows\DWM"; Name="AlwaysHibernateThumbnails"; Value=0},
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="TaskbarAnimations"; Value=1},
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="IconsOnly"; Value=0},
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="ListviewShadow"; Value=1},
+	@{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="ListviewAlphaSelect"; Value=1},
 
-Write-Host "[visual_effects] Restoring taskbar + listview animations..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAnimations" -Value 1 -Type DWord -Force -ErrorAction SilentlyContinue
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewShadow" -Value 1 -Type DWord -Force -ErrorAction SilentlyContinue
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewAlphaSelect" -Value 1 -Type DWord -Force -ErrorAction SilentlyContinue
+	# Misc.
+    @{Path="HKCU:\Control Panel\Keyboard"; Name="KeyboardDelay"; Value="1"},
+	@{Path="HKCU:\Control Panel\Mouse"; Name="MouseHoverTime"; Value="400"},
+	@{Path="HKCU:\Control Panel\Desktop"; Name="CursorBlinkRate"; Value="530"},
+	@{Path="HKCU:\Control Panel\Desktop"; Name="MenuShowDelay"; Value="400"}
+)
+
+foreach ($item in $perfValues) {
+    New-Item -Path $item.Path -Force -ErrorAction SilentlyContinue | Out-Null
+    Set-ItemProperty -Path $item.Path -Name $item.Name -Value $item.Value -Force -ErrorAction SilentlyContinue
+}
